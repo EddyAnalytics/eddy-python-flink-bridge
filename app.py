20149e50fd29a225fb24b4fe3d8c804eac2a9873
Maurits van der Vijgh <maurits@elnino.tech>
2019-10-16 14:39:46 +0200

    fix things

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Celery('app', broker="redis://{}:6379".format(config.REDIS_HOST))
 
 @app.task
 def submit_flink_sql(definition):
-    logging.info(args)
+    logging.info(definition)
     process = subprocess.Popen(['./docker-entrypoint.sh', 'python3', 'sql.py', definition], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     logging.info(stdout)
