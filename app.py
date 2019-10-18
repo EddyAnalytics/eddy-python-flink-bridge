ace630300f1540802adba8fe035ce93d465e93c4
Maurits van der Vijgh <maurits@elnino.tech>
2019-10-18 11:57:29 +0200

    Add feedback to kafka hopefully

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from kafka import KafkaProducer
 from collections import OrderedDict
 import subprocess
 import config
@@ -12,8 +13,23 @@ def submit_flink_sql(definition):
     logging.info(definition)
     process = subprocess.Popen(['./docker-entrypoint.sh', 'python3', 'sql.py', definition], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
+
+    definition = json.loads(definition)
+    feedback = {
+	"id": definition["id"],
+        "success": bool(stderr)
+    }
+
     logging.info(stdout)
     if stderr:
+        feedback["error"] = stderr
         logging.error(stderr)
+    else:
+        feedback["jobId"] = stdout
+
+    producer = KafkaProducer(bootstrap_servers=config.BOOTSTRAP_SERVERS)
+    future = producer.send("{}.{}.feedback".format(feedback["projectId"], feedback["pipelineId"]), json.dumps(feedback).encode('utf-8'))
+    result = future.get(timeout=10)
+
     return (stdout, stderr)
 
